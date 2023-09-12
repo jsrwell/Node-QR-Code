e0b109a310102c5c4ddc4284b798b677969fcacd
Wellington Jackson <wellingtonjsramos@hotmail.com>
2023-09-11 21:39:25 -0300

    Project Finished - QR-Code Creator; - Write an .txt with the text of QR-Code too.

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-/* 
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+inquirer
+    .prompt([{
+        message: "Insert the data you want to transform in a QR-Code:",
+        name: "URL"
+    },
+    {
+        message: "Inser the name of the image:",
+        name: "name"
+    }])
+    .then((answers) => {
+        // Create QR-Code Based on Answers
+        var qr_svg = qr.image(answers.URL);
+        qr_svg.pipe(fs.createWriteStream(`${answers.name}.png`));
+
+        // Write a file with the text of QR-Code
+        fs.writeFile(`${answers.name}.txt`, data, (err) => {
+            if (err) throw err;
+        })
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
